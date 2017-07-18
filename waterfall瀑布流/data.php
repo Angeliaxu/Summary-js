1b59c115932d76d06d63620ef80d678f13eeae6f
Angeliaxu <2656995425@qq.com>
2017-07-18 23:01:35 +0800

    review

--- waterfall瀑布流/data.php
+++ waterfall瀑布流/data.php
@@ -10,8 +10,11 @@
  * */
 //isset函数判断一个变量是否被定义过	
 $cpage=isset($_GET['cpage'])? $_GET['cpage']:1;
+//获取数据的url地址，.的意思是链接字符串。
 $url='http://www.wookmark.com/api/json/popular?page='.$cpage;
+//把整个文件读入一个字符串中。
 $content=file_get_contents($url);
+//转化编码从gbk格式转换成utf-8格式。
 $context=iconv('gbk','utf-8',$content);
-echo $content;
+echo $context;
 ?>
